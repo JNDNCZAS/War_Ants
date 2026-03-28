570d18e7ba0770de291b052288c0bff143ca55fe
Jhon <jhoncani13@gmail.com>
2026-03-27 19:26:18 -0500

    Cree un sistema de 3 tilemaps, en el del medio se podran las cosas que alterar la maya de navegacion, en la de arriba las cosas de decoracion que no influyen en nada, y en la de abajo se pone un fondo para los objetos de la segunda capa

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,7 +1,6 @@
 extends Camera2D
 
 @export var speed: float = 600.0
-@export var margin: float = 120.0
 @export var map_limit_left: float = 0.0
 @export var map_limit_top: float = 0.0
 @export var map_limit_right: float = 4096.0
@@ -9,7 +8,7 @@ extends Camera2D
 
 @export var zoom_speed: float = 0.1
 @export var zoom_min: float = 0.5
-@export var zoom_max: float = 3
+@export var zoom_max: float = 2.0
 
 var _limit_left: float
 var _limit_top: float
@@ -29,33 +28,28 @@ func _input(event):
 			var mouse_screen = get_viewport().get_mouse_position()
 			var screen_center = get_viewport().get_visible_rect().size / 2.0
 			var offset = mouse_screen - screen_center
-			
 			var zoom_before = zoom.x
-			
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 				zoom = clamp(zoom + Vector2.ONE * zoom_speed, Vector2.ONE * zoom_min, Vector2.ONE * zoom_max)
 			else:
 				zoom = clamp(zoom - Vector2.ONE * zoom_speed, Vector2.ONE * zoom_min, Vector2.ONE * zoom_max)
-			
 			var zoom_after = zoom.x
 			var zoom_factor = (1.0 / zoom_before) - (1.0 / zoom_after)
 			position += offset * zoom_factor
-
 			position.x = clamp(position.x, _limit_left, _limit_right)
 			position.y = clamp(position.y, _limit_top, _limit_bottom)
 
 func _process(delta):
-	var mouse = get_viewport().get_mouse_position()
-	var screen = get_viewport().get_visible_rect().size
 	var move = Vector2.ZERO
 
-	if mouse.x < margin:
+	if Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A):
 		move.x = -1
-	elif mouse.x > screen.x - margin:
+	elif Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D):
 		move.x = 1
-	if mouse.y < margin:
+
+	if Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_W):
 		move.y = -1
-	elif mouse.y > screen.y - margin:
+	elif Input.is_action_pressed("ui_down") or Input.is_key_pressed(KEY_S):
 		move.y = 1
 
 	if move != Vector2.ZERO:
